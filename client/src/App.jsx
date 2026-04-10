09dc80f2328223c0f44b3f55ba4b591141c7ffd3
Francisco Lazarte Salazar <202401362@est.umss.edu>
2026-04-10 11:36:17 -0400

    Configuracion de react

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateAulaForm from './components/CreateAulaForm/CreateAulaForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-main">
+      <CreateAulaForm />
+    </div>
+  );
+}
+
+export default App;
